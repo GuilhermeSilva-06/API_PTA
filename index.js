ac2eb4560cb106289893bae5c8d9d29316fa97e0
GUILHERME FERNANDES SILVA <guilherme.silva231@cs.cruzeirodosul.edu.br>
2026-09-18 21:28:52 -0300

    rota, controller e Service criado e testado no Postman

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const chamadoRoutes = require('./routes/chamadoRoutes')
+const PORT = 3002;
+
+app.get('/,')
 
 app.use(express.json());
 
+app.use('/chamados', chamadoRoutes);
+
 app.listen(PORT, function(){
     console.log('servidor rodando na porta '+ PORT);
-});
+})
